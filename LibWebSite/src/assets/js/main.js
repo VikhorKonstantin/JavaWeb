238eb4017e07fd16a764c586d23e1cbf18a29c20
VikhorKonstantin <konstantin.vikhor@gmail.com>
2019-08-01 01:43:04 +0300

    add LibWebSite

---
+++ LibWebSite/src/assets/js/main.js
@@ -0,0 +1,7 @@
+//=require ../../../node_modules/jquery/dist/jquery.js
+//=require ../../../node_modules/popper.js/dist/umd/popper.js
+//=require ../../../node_modules/bootstrap/js/dist/util.js
+//=require ../../../node_modules/bootstrap/js/dist/tooltip.js
+//=require ../../../node_modules/bootstrap/js/dist/popover.js
+//=require ../../../node_modules/bootstrap/js/dist/collapse.js
+//=require ../../../node_modules/bootstrap/js/dist/carousel.js
