31177f9a20adde667c378a91c0e09877cd664467
VikhorKonstantin <konstantin.vikhor@gmail.com>
2019-08-18 23:37:59 +0300

    Internationalization added.

--- Task3Composite/src/by/training/task3composite/view/Runner.java
+++ Task3Composite/src/by/training/task3composite/view/Runner.java
@@ -4,24 +4,23 @@ import by.training.task3composite.controller.Controller;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
 import java.util.Scanner;
+import java.util.StringJoiner;
 
 public final class Runner {
     /**
-     * commands information.
+     * Choose language message.
      */
-    private static final String COMMANDS_INFO =
-            "Command\tArgs\n"
-            + "PARSE\tFILENAME\n"
-            + "SORT_PARAGRAPH\tFILENAME\n"
-            + "SORT_SENTENCES\tFILENAME\n"
-            + "SORT_WORDS\tFILENAME\n"
-            + "SORT_LEXEMES\tFILENAME CHAR\n";
-
+    private static final String CHOOSE_LANG =
+            "1 — английский\n2 — белорусский\nЛюбой — русский";
     /**
      * Logger.
      */
     private static Logger logger = LogManager.getLogger("STDOUT");
+
+
     private Runner() {
     }
 
@@ -33,12 +32,87 @@ public final class Runner {
         var scanner = new Scanner(System.in);
         String command;
         String response;
-        logger.info(COMMANDS_INFO);
+        logger.info(CHOOSE_LANG);
+        String chosenNumber = scanner.nextLine();
+        String country = "";
+        String language = "";
+        switch (chosenNumber) {
+            case "1":
+                country = "US";
+                language = "EN";
+                break;
+            case "2":
+                country = "BY";
+                language = "BE";
+                break;
+            default:
+        }
+        Locale current = new Locale(language, country);
+        ResourceManager.INSTANCE.changeResource(current);
+        final String commandsInfo = createCommandsInfo();
+        logger.info(commandsInfo);
         command = scanner.nextLine();
         while (!command.equals("EXIT")) {
             response = controller.executeTask(command);
             logger.info(response);
             command = scanner.nextLine();
+        }
+    }
+
+    private static String createCommandsInfo() {
+        ResourceManager resourceManager = ResourceManager.INSTANCE;
+        final String startInfoKey = "startInfo";
+        final String startInfo = resourceManager.getString(startInfoKey);
+        final String fileNameKey = "nameOfFile";
+        final String fileName = resourceManager.getString(fileNameKey);
+        final String charKey = "character";
+        final String character = resourceManager.getString(charKey);
+        StringJoiner joiner = new StringJoiner("\n");
+        joiner.add(startInfo).add("PARSE " + fileName)
+                .add("SORT_PARAGRAPH\t" + fileName)
+                .add("SORT_SENTENCES\t" + fileName)
+                .add("SORT_WORDS\t" + fileName)
+                .add("SORT_LEXEMES\t" + fileName + '\t' + character);
+        return joiner.toString();
+    }
+
+    private enum ResourceManager {
+        /**
+         * Instance.
+         */
+        INSTANCE;
+        /**
+         * Base name of resources.
+         */
+        private final String resourceName = "property.text";
+        /**
+         * Instance of ResourceBundle.
+         */
+        private ResourceBundle resourceBundle;
+
+        /**
+         * Creates new ResourceManager.
+         */
+        ResourceManager() {
+            resourceBundle = ResourceBundle.getBundle(resourceName,
+                    Locale.getDefault());
+        }
+
+        /**
+         * Changes locale.
+         * @param locale new locale
+         */
+        public void changeResource(final Locale locale) {
+            resourceBundle = ResourceBundle.getBundle(resourceName, locale);
+        }
+
+        /**
+         * Returns String by key.
+         * @param key key of required String
+         * @return String
+         */
+        public String getString(final String key) {
+            return resourceBundle.getString(key);
         }
     }
 }
